d5d80ad92841eb8d8e05bde87fd911c577ad0b19
mlee03 <Mikyung.Lee@lscamd50-d.gfdl.noaa.gov>
2025-09-02 09:25:31 -0400

    initial name changes

--- c_horiz_interp/c_horiz_interp.F90
+++ c_horiz_interp/c_horiz_interp.F90
@@ -46,6 +46,7 @@ module c_horiz_interp_mod
   type(FmsHorizInterp_type), allocatable, target, public :: interp(:)
   type(fmshorizinterp_type), pointer :: current_interp
   integer, public :: current_interp_id
+  integer :: interp_count = 0
   
 contains
 
