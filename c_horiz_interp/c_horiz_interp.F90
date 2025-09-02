8c79229607cbca30a03012ee7e76ca049d2c4f5e
mlee03 <Mikyung.Lee@lscamd50-d.gfdl.noaa.gov>
2025-09-02 15:20:11 -0400

    update tests

--- c_horiz_interp/c_horiz_interp.F90
+++ c_horiz_interp/c_horiz_interp.F90
@@ -19,10 +19,9 @@ module c_horiz_interp_mod
   public :: cFMS_get_maxxgrid
   public :: cFMS_horiz_interp_init
   public :: cFMS_horiz_interp_dealloc
-  public :: cFMS_set_current_interp
 
-  public :: cFMS_horiz_interp_2d_cdouble
-  public :: cFMS_horiz_interp_2d_cfloat
+  public :: cFMS_horiz_interp_get_weights_2d_cdouble
+  public :: cFMS_horiz_interp_get_weights_2d_cfloat
 
   public :: cFMS_get_interp_cdouble
   public :: cFMS_get_interp_cfloat
@@ -33,7 +32,6 @@ module c_horiz_interp_mod
   public :: cFMS_get_nlon_dst, cFMS_get_nlat_dst
   public :: cFMS_get_version
   public :: cFMS_get_nxgrid
-  public :: cFMS_get_current_interp
   public :: cFMS_get_interp_method
 
   public :: cFMS_get_i_lon
@@ -104,29 +102,6 @@ contains
 
   end subroutine cFMS_horiz_interp_init
 
-  ! cFMS_get_current_interp
-  function cFMS_get_current_interp()
-    implicit none
-    type(FmsHorizInterp_type), pointer :: cFMS_get_current_interp
-    cFMS_get_current_interp => current_interp
-  end function cFMS_get_current_interp
-
-  !cFMS_set_current_interp
-  subroutine cFMS_set_current_interp(interp_id) bind(C, name="cFMS_set_current_interp")
-
-    implicit none
-    integer, intent(in), optional :: interp_id
-    
-    if(present(interp_id)) then
-       current_interp => interp(interp_id)
-       current_interp_id = interp_id
-    else
-       current_interp => interp(0)
-       current_interp_id = 0
-    end if
-    
-  end subroutine cFMS_set_current_interp
-
   !cFMS_horiz_interp_dealloc
   subroutine cFMS_horiz_interp_dealloc() bind(C, name="cFMS_horiz_interp_dealloc")
     implicit none
@@ -136,86 +111,8 @@ contains
 
   end subroutine cFMS_horiz_interp_dealloc
 
-  subroutine cFMS_get_i_src(interp_id, i_src) bind(C, name="cFMS_get_i_src")
-      integer, intent(in) :: interp_id
-      integer, intent(out):: i_src(interp(interp_id)%nxgrid)
-      i_src = interp(interp_id)%i_src
-  end subroutine cFMS_get_i_src
-
-  subroutine cFMS_get_j_src(interp_id, j_src) bind(C, name="cFMS_get_j_src")
-      integer, intent(in) :: interp_id
-      integer, intent(out) :: j_src(interp(interp_id)%nxgrid)
-      j_src = interp(interp_id)%j_src
-  end subroutine cFMS_get_j_src
-
-  subroutine cFMS_get_i_lon(interp_id, i_lon) bind(C, name="cFMS_get_i_lon")
-      integer, intent(in) :: interp_id
-      integer, intent(out) :: i_lon(interp(interp_id)%nlon_dst, interp(interp_id)%nlat_dst, 2)
-      i_lon = interp(interp_id)%i_lon
-  end subroutine cFMS_get_i_lon
-
-  subroutine cFMS_get_j_lat(interp_id, j_lat) bind(C, name="cFMS_get_j_lat")
-      integer, intent(in) :: interp_id
-      integer :: j_lat(interp(interp_id)%nlon_dst, interp(interp_id)%nlat_dst, 2)
-      j_lat = interp(interp_id)%j_lat
-  end subroutine cfms_get_j_lat
-
-  subroutine cFMS_get_i_dst(interp_id, i_dst) bind(C, name="cFMS_get_i_dst")
-      integer, intent(in) :: interp_id
-      integer :: i_dst(interp(interp_id)%nxgrid)
-      i_dst = interp(interp_id)%i_dst
-  end subroutine cFMS_get_i_dst
-
-  subroutine cFMS_get_j_dst(interp_id, j_dst) bind(C, name="cFMS_get_j_dst")
-      integer, intent(in) :: interp_id
-      integer :: j_dst(interp(interp_id)%nxgrid)
-      j_dst = interp(interp_id)%j_dst
-  end subroutine cFMS_get_j_dst
-
-  subroutine cFMS_get_version(interp_id, version) bind(C, name="cFMS_get_version")
-      integer, intent(in) :: interp_id
-      integer, intent(out) :: version
-      version = interp(interp_id)%version
-  end subroutine cFMS_get_version
-
-  subroutine cFMS_get_nxgrid(interp_id, nxgrid) bind(C, name="cFMS_get_nxgrid")
-      integer, intent(in) :: interp_id
-      integer, intent(out) :: nxgrid
-      nxgrid = interp(interp_id)%nxgrid
-  end subroutine cFMS_get_nxgrid
-
-  subroutine cFMS_get_nlon_src(interp_id, nlon_src) bind(C, name="cFMS_get_nlon_src")
-      integer, intent(in) :: interp_id
-      integer, intent(out) :: nlon_src
-      nlon_src = interp(interp_id)%nlon_src
-  end subroutine cFMS_get_nlon_src
-
-  subroutine cFMS_get_nlat_src(interp_id, nlat_src) bind(C, name="cFMS_get_nlat_src")
-      integer, intent(in) :: interp_id
-      integer, intent(out) :: nlat_src
-      nlat_src = interp(interp_id)%nlat_src
-  end subroutine cFMS_get_nlat_src
-
-  subroutine cFMS_get_nlon_dst(interp_id, nlon_dst) bind(C, name="cFMS_get_nlon_dst")
-      integer, intent(in) :: interp_id
-      integer, intent(out) :: nlon_dst
-      nlon_dst = interp(interp_id)%nlon_dst
-  end subroutine cFMS_get_nlon_dst
-
-  subroutine cFMS_get_nlat_dst(interp_id, nlat_dst) bind(C, name="cFMS_get_nlat_dst")
-      integer, intent(in) :: interp_id
-      integer, intent(out) :: nlat_dst
-      nlat_dst = interp(interp_id)%nlat_dst
-  end subroutine cFMS_get_nlat_dst
-
-  subroutine cFMS_get_interp_method(interp_id, interp_method) bind(C, name="cFMS_get_interp_method")
-      integer, intent(in) :: interp_id
-      integer, intent(out) :: interp_method
-      interp_method = interp(interp_id)%interp_method
-  end subroutine cFMS_get_interp_method
-
-
-#include "c_horiz_interp_new.fh"
+#include "c_horiz_interp_int.inc"
+#include "c_horiz_interp_get_weights.fh"
 #include "c_get_interp.fh"
   
 end module c_horiz_interp_mod
--- c_horiz_interp/c_horiz_interp.h
+++ c_horiz_interp/c_horiz_interp.h
@@ -16,29 +16,27 @@ extern void cFMS_horiz_interp_dealloc();
 
 extern void cFMS_set_current_interp(int *interp_id);
 
-extern int cFMS_horiz_interp_2d_cfloat(float *lon_in_ptr, int *lon_in_shape, float *lat_in_ptr,
-                                            int *lat_in_shape, float *lon_out_ptr, int *lon_out_shape,
-                                            float *lat_out_ptr, int *lat_out_shape, char *interp_method,
-                                            int *verbose, float *max_dist, bool *src_modulo, 
-                                            float *mask_in_ptr, float *mask_out_ptr, bool *is_latlon_in, 
-                                            bool *is_latlon_out);
+extern int cFMS_horiz_interp_get_weights_2d_cfloat(float *lon_in_ptr,float *lat_in_ptr, int *lonlat_in_shape, 
+                                                   float *lon_out_ptr, float *lat_out_ptr, int *lonlat_out_shape, char *interp_method,
+                                                   int *verbose, float *max_dist, bool *src_modulo, 
+                                                   float *mask_in_ptr, float *mask_out_ptr, bool *is_latlon_in, 
+                                                   bool *is_latlon_out);
 
-extern int cFMS_horiz_interp_2d_cdouble(double *lon_in_ptr, int *lon_in_shape, double *lat_in_ptr,
-                                            int *lat_in_shape, double *lon_out_ptr, int *lon_out_shape,
-                                            double *lat_out_ptr, int *lat_out_shape, char *interp_method,
-                                            int *verbose, double *max_dist, bool *src_modulo, 
-                                            double *mask_in_ptr, double *mask_out_ptr, bool *is_latlon_in, 
-                                            bool *is_latlon_out);
+extern int cFMS_horiz_interp_get_weights_2d_cdouble(double *lon_in_ptr,double *lat_in_ptr, int *lonlat_in_shape, 
+                                                   double *lon_out_ptr, double *lat_out_ptr, int *lonlat_out_shape, char *interp_method,
+                                                   int *verbose, double *max_dist, bool *src_modulo, 
+                                                   double *mask_in_ptr, double *mask_out_ptr, bool *is_latlon_in, 
+                                                   bool *is_latlon_out);
 
 extern void cFMS_get_interp_cfloat(int *interp_id, int *i_src, int *j_src, int *i_dst, int *j_dst, 
-                                            float *area_frac_dst, int *version, int *nxgrid, 
-                                            int *nlon_src, int *nlat_src, int *nlon_dst, int *nlat_dst, 
-                                            bool *is_allocated, int *interp_method);
+                                   float *area_frac_dst, int *version, int *nxgrid, 
+                                   int *nlon_src, int *nlat_src, int *nlon_dst, int *nlat_dst, 
+                                   bool *is_allocated, int *interp_method);
                                    
 extern void cFMS_get_interp_cdouble(int *interp_id, int *i_src, int *j_src, int *i_dst, int *j_dst, 
-                                            double *area_frac_dst, int *version, int *nxgrid, 
-                                            int *nlon_src, int *nlat_src, int *nlon_dst, int *nlat_dst, 
-                                            bool *is_allocated, int *interp_method);
+                                    double *area_frac_dst, int *version, int *nxgrid, 
+                                    int *nlon_src, int *nlat_src, int *nlon_dst, int *nlat_dst, 
+                                    bool *is_allocated, int *interp_method);
 
 // Integer pointer getters
 extern void cFMS_get_i_src(int *interp_id, int *i_src);
