1c801fdcac36a45aa5c38ff585748d8e99f48fe3
Frank Malatino <frank.malatino@noaa.gov>
2025-09-25 14:57:57 -0400

    Working conda build method

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -2,43 +2,30 @@
 
 set -ex
 
-FC=mpif90
+FC=mpifort
 CC=mpicc
 
-CFLAGS="${CFLAGS} -fPIC -I${PREFIX}/include"
-FCFLAGS="${FCFLAGS} -fPIC -I${PREFIX}/include"
-LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
-
+CFLAGS="-fPIC -I${PREFIX}/include"
+FCFLAGS="-fPIC -I${PREFIX}/include"
+LDFLAGS="-L${PREFIX}/lib"
 
 curr_dir=$PWD
 
-fms_install=${PREFIX}/lib/FMS
-cfms_install=${PREFIX}/lib/cFMS
+fms_install="${PREFIX}"
+cfms_install="${PREFIX}"
 
 cfms_dir=$curr_dir/cFMS
 fms_dir=$curr_dir/FMS
 
-cd $fms_dir
-autoreconf -iv
-./configure --enable-portable-kinds \
-            --with-yaml \
-            --prefix=$fms_install \
-            FC=$FC \
-            CC=$CC \
-            FCFLAGS="$FCFLAGS" \
-            CFLAGS="$CFLAGS" \
-            LDFLAGS="$LDFLAGS"
-make install
+# cd $fms_dir
+# autoreconf -iv
+# ./configure --enable-portable-kinds \
+#             --with-yaml \
+#             --prefix=$fms_install
+# make install
 
 cd $curr_dir
-
-cd $cfms_dir
 autoreconf -iv
 ./configure --with-fms=$fms_install \
-            --prefix=$cfms_install \
-            FC=$FC \
-            CC=$CC \
-            FCFLAGS="$FCFLAGS" \
-            CFLAGS="$CFLAGS" \
-            LDFLAGS="$LDFLAGS"
+            --prefix=$cfms_install
 make install
