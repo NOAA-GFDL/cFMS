16926237434495b56a595bbc30f0e121a357982b
mlee03 <Mikyung.Lee@lscamd50-d.gfdl.noaa.gov>
2025-11-14 14:51:10 -0500

    get_xgrid_area

--- c_horiz_interp/c_horiz_interp.F90
+++ c_horiz_interp/c_horiz_interp.F90
@@ -1,161 +1,163 @@
 module c_horiz_interp_mod
 
-  use FMS, only : fms_horiz_interp_init, fms_horiz_interp_del
-  use FMS, only : FmsHorizInterp_type
-  use FMS, only : fms_horiz_interp_new, fms_horiz_interp_read_weights_conserve
-  use FMS, only : fms_horiz_interp
-  use FMS, only : fms_string_utils_c2f_string
-  use FMS, only : fms_mpp_error, WARNING
+   use FMS, only : fms_horiz_interp_init, fms_horiz_interp_del
+   use FMS, only : FmsHorizInterp_type
+   use FMS, only : fms_horiz_interp_new, fms_horiz_interp_read_weights_conserve
+   use FMS, only : fms_horiz_interp
+   use FMS, only : fms_string_utils_c2f_string
+   use FMS, only : fms_mpp_error, WARNING
 
-  use c_fms_utils_mod, only : cFMS_pointer_to_array, cFMS_array_to_pointer
-  use c_fms_mod, only : NAME_LENGTH, MESSAGE_LENGTH
+   use c_fms_utils_mod, only : cFMS_pointer_to_array, cFMS_array_to_pointer
+   use c_fms_mod, only : NAME_LENGTH, MESSAGE_LENGTH
 
-  use iso_c_binding
+   use iso_c_binding
 
-  implicit none
+   implicit none
 
-  private
+   private
 
-  public :: cFMS_create_xgrid_2dx2d_order1
-  public :: cFMS_get_maxxgrid
-  public :: cFMS_horiz_interp_init
-  public :: cFMS_horiz_interp_end
+   public :: cFMS_create_xgrid_2dx2d_order1
+   public :: cFMS_get_maxxgrid
+   public :: cFMS_horiz_interp_init
+   public :: cFMS_horiz_interp_end
 
-  public :: cFMS_horiz_interp_new_2d_cdouble
-  public :: cFMS_horiz_interp_new_2d_cfloat
+   public :: cFMS_horiz_interp_new_2d_cdouble
+   public :: cFMS_horiz_interp_new_2d_cfloat
 
-  public :: cFMS_horiz_interp_base_2d_cfloat
-  public :: cFMS_horiz_interp_base_2d_cdouble
+   public :: cFMS_horiz_interp_base_2d_cfloat
+   public :: cFMS_horiz_interp_base_2d_cdouble
 
-  public :: cFMS_horiz_interp_read_weights_conserve
+   public :: cFMS_horiz_interp_read_weights_conserve
 
-  public :: cFMS_get_i_src, cFMS_get_i_dst
-  public :: cFMS_get_j_src, cFMS_get_j_dst
-  public :: cFMS_get_nlon_src, cFMS_get_nlat_src
-  public :: cFMS_get_nlon_dst, cFMS_get_nlat_dst
-  public :: cFMS_get_version
-  public :: cFMS_get_nxgrid
-  public :: cFMS_get_interp_method
+   public :: cFMS_get_i_src, cFMS_get_i_dst
+   public :: cFMS_get_j_src, cFMS_get_j_dst
+   public :: cFMS_get_nlon_src, cFMS_get_nlat_src
+   public :: cFMS_get_nlon_dst, cFMS_get_nlat_dst
+   public :: cFMS_get_version
+   public :: cFMS_get_nxgrid
+   public :: cFMS_get_interp_method
 
-  public :: cFMS_get_i_lon
-  public :: cFMS_get_j_lat
-  public :: cFMS_get_area_frac_dst_double
-  public :: cFMS_get_area_frac_dst_float
-  public :: cFMS_get_is_allocated_double
-  public :: cFMS_get_is_allocated_float
+   public :: cFMS_get_i_lon
+   public :: cFMS_get_j_lat
+   public :: cFMS_get_area_frac_dst_cdouble
+   public :: cFMS_get_area_frac_dst_cfloat
+   public :: cFMS_get_is_allocated_cdouble
+   public :: cFMS_get_is_allocated_cfloat
+   public :: cFMS_get_xgrid_area_cdouble
+   public :: cFMS_get_xgrid_area_cfloat
 
-  type(FmsHorizInterp_type), allocatable, target, public :: interp(:)
-  integer :: interp_count = 0
+   type(FmsHorizInterp_type), allocatable, target, public :: interp(:)
+   integer :: interp_count = 0
 
-  logical :: module_is_initialized = .false.
-  
+   logical :: module_is_initialized = .false.
+
 contains
 
-  !cFMS_create_xgrid_2dx2d_order1
-  function cFMS_create_xgrid_2dx2d_order1(nlon_in, nlat_in, nlon_out, nlat_out, lon_in, lat_in, lon_out, &
-       lat_out, mask_in, maxxgrid, i_in, j_in, i_out, j_out, xgrid_area) bind(C, name="cFMS_create_xgrid_2dx2d_order1")
+   !cFMS_create_xgrid_2dx2d_order1
+   function cFMS_create_xgrid_2dx2d_order1(nlon_in, nlat_in, nlon_out, nlat_out, lon_in, lat_in, lon_out, &
+      lat_out, mask_in, maxxgrid, i_in, j_in, i_out, j_out, xgrid_area) bind(C, name="cFMS_create_xgrid_2dx2d_order1")
 
-    integer, intent(in) :: nlon_in
-    integer, intent(in) :: nlat_in
-    integer, intent(in) :: nlon_out
-    integer, intent(in) :: nlat_out
-    real(c_double), intent(in) :: lon_in((nlon_in+1)*(nlat_in+1))
-    real(c_double), intent(in) :: lat_in((nlon_in+1)*(nlat_in+1))
-    real(c_double), intent(in) :: lon_out((nlon_out+1)*(nlat_out+1))
-    real(c_double), intent(in) :: lat_out((nlon_out+1)*(nlat_out+1))
-    real(c_double), intent(in) :: mask_in(nlon_in*nlat_in)
-    integer, intent(in) :: maxxgrid
-    real(c_double), intent(out) :: i_in(maxxgrid)
-    real(c_double), intent(out) :: j_in(maxxgrid)
-    real(c_double), intent(out) :: i_out(maxxgrid)
-    real(c_double), intent(out) :: j_out(maxxgrid)
-    real(c_double), intent(out) :: xgrid_area(maxxgrid)
+      integer, intent(in) :: nlon_in
+      integer, intent(in) :: nlat_in
+      integer, intent(in) :: nlon_out
+      integer, intent(in) :: nlat_out
+      real(c_double), intent(in) :: lon_in((nlon_in+1)*(nlat_in+1))
+      real(c_double), intent(in) :: lat_in((nlon_in+1)*(nlat_in+1))
+      real(c_double), intent(in) :: lon_out((nlon_out+1)*(nlat_out+1))
+      real(c_double), intent(in) :: lat_out((nlon_out+1)*(nlat_out+1))
+      real(c_double), intent(in) :: mask_in(nlon_in*nlat_in)
+      integer, intent(in) :: maxxgrid
+      real(c_double), intent(out) :: i_in(maxxgrid)
+      real(c_double), intent(out) :: j_in(maxxgrid)
+      real(c_double), intent(out) :: i_out(maxxgrid)
+      real(c_double), intent(out) :: j_out(maxxgrid)
+      real(c_double), intent(out) :: xgrid_area(maxxgrid)
 
-    integer create_xgrid_2dx2d_order1
-    integer cFMS_create_xgrid_2dx2d_order1
+      integer create_xgrid_2dx2d_order1
+      integer cFMS_create_xgrid_2dx2d_order1
 
-    cFMS_create_xgrid_2dx2d_order1 = create_xgrid_2dx2d_order1(nlon_in, nlat_in, nlon_out, nlat_out, lon_in, lat_in, &
+      cFMS_create_xgrid_2dx2d_order1 = create_xgrid_2dx2d_order1(nlon_in, nlat_in, nlon_out, nlat_out, lon_in, lat_in, &
          lon_out, lat_out, mask_in, i_in, j_in, i_out, j_out, xgrid_area)
 
-  end function cFMS_create_xgrid_2dx2d_order1
+   end function cFMS_create_xgrid_2dx2d_order1
 
-  !cFMS_get_maxxgrid
-  function cFMS_get_maxxgrid() bind(C, name="cFMS_get_maxxgrid")
+   !cFMS_get_maxxgrid
+   function cFMS_get_maxxgrid() bind(C, name="cFMS_get_maxxgrid")
 
-    implicit none
-    integer :: get_maxxgrid
-    integer :: cFMS_get_maxxgrid
+      implicit none
+      integer :: get_maxxgrid
+      integer :: cFMS_get_maxxgrid
 
-    cFMS_get_maxxgrid = get_maxxgrid()
+      cFMS_get_maxxgrid = get_maxxgrid()
 
-  end function cFMS_get_maxxgrid
+   end function cFMS_get_maxxgrid
 
-  !cFMS_horiz_interp_init
-  subroutine cFMS_horiz_interp_init(ninterp) bind(C, name="cFMS_horiz_interp_init")
-    implicit none
-    integer, intent(in), optional :: ninterp
+   !cFMS_horiz_interp_init
+   subroutine cFMS_horiz_interp_init(ninterp) bind(C, name="cFMS_horiz_interp_init")
+      implicit none
+      integer, intent(in), optional :: ninterp
 
-    call fms_horiz_interp_init()
+      call fms_horiz_interp_init()
 
-    if(module_is_initialized) return
-    
-    if(present(ninterp)) then
-      allocate(interp(0:ninterp-1))
-    else
-      allocate(interp(0:0))
-   end if
+      if(module_is_initialized) return
 
-   interp_count = 0
+      if(present(ninterp)) then
+         allocate(interp(0:ninterp-1))
+      else
+         allocate(interp(0:0))
+      end if
 
-   module_is_initialized = .true.
+      interp_count = 0
 
-  end subroutine cFMS_horiz_interp_init
+      module_is_initialized = .true.
 
-  !cFMS_horiz_interp_end
-  subroutine cFMS_horiz_interp_end() bind(C, name="cFMS_horiz_interp_end")
-    implicit none
-    integer :: i
+   end subroutine cFMS_horiz_interp_init
 
-    do i=0, size(interp)-1
-       call fms_horiz_interp_del(interp(i))
-    end do
-    deallocate(interp)
+   !cFMS_horiz_interp_end
+   subroutine cFMS_horiz_interp_end() bind(C, name="cFMS_horiz_interp_end")
+      implicit none
+      integer :: i
 
-    module_is_initialized = .false.
-    
-  end subroutine cFMS_horiz_interp_end
+      do i=0, size(interp)-1
+         call fms_horiz_interp_del(interp(i))
+      end do
+      deallocate(interp)
 
-  function cFMS_horiz_interp_read_weights_conserve(weight_filename, weight_file_source, &
-       nlon_src, nlat_src, nlon_dst, nlat_dst, isw, iew, jsw, jew, src_tile) &
-       bind(C, name="cFMS_horiz_interp_read_weights_conserve")
+      module_is_initialized = .false.
 
-    implicit none
+   end subroutine cFMS_horiz_interp_end
 
-    character(c_char), intent(in) :: weight_filename(NAME_LENGTH)
-    character(c_char), intent(in) :: weight_file_source(NAME_LENGTH)
-    integer, intent(in) :: nlon_src, nlat_src, nlon_dst, nlat_dst
-    integer, intent(in) :: isw, iew, jsw, jew
-    integer, intent(in) :: src_tile
+   function cFMS_horiz_interp_read_weights_conserve(weight_filename, weight_file_source, &
+      nlon_src, nlat_src, nlon_dst, nlat_dst, isw, iew, jsw, jew, src_tile) &
+      bind(C, name="cFMS_horiz_interp_read_weights_conserve")
 
-    character(NAME_LENGTH-1) :: weight_filename_f
-    character(NAME_LENGTH-1) :: weight_file_source_f
+      implicit none
 
-    integer :: interp_id
-    integer :: cFMS_horiz_interp_read_weights_conserve ! interp id
+      character(c_char), intent(in) :: weight_filename(NAME_LENGTH)
+      character(c_char), intent(in) :: weight_file_source(NAME_LENGTH)
+      integer, intent(in) :: nlon_src, nlat_src, nlon_dst, nlat_dst
+      integer, intent(in) :: isw, iew, jsw, jew
+      integer, intent(in) :: src_tile
 
-    weight_filename_f = fms_string_utils_c2f_string(weight_filename)
-    weight_file_source_f = fms_string_utils_c2f_string(weight_file_source)
+      character(NAME_LENGTH-1) :: weight_filename_f
+      character(NAME_LENGTH-1) :: weight_file_source_f
 
-    interp_id = interp_count
+      integer :: interp_id
+      integer :: cFMS_horiz_interp_read_weights_conserve ! interp id
 
-    call fms_horiz_interp_read_weights_conserve(interp(interp_id), weight_filename_f, weight_file_source_f, &
+      weight_filename_f = fms_string_utils_c2f_string(weight_filename)
+      weight_file_source_f = fms_string_utils_c2f_string(weight_file_source)
+
+      interp_id = interp_count
+
+      call fms_horiz_interp_read_weights_conserve(interp(interp_id), weight_filename_f, weight_file_source_f, &
          nlon_src, nlat_src, nlon_dst, nlat_dst, isw+1, iew+1, jsw+1, jew+1, src_tile)
 
-    cFMS_horiz_interp_read_weights_conserve = interp_id
+      cFMS_horiz_interp_read_weights_conserve = interp_id
 
-    interp_count = interp_count + 1
+      interp_count = interp_count + 1
 
-  end function cFMS_horiz_interp_read_weights_conserve
+   end function cFMS_horiz_interp_read_weights_conserve
 
 #include "c_horiz_interp_int.inc"
 #include "c_horiz_interp_new.fh"
--- c_horiz_interp/c_horiz_interp.h
+++ c_horiz_interp/c_horiz_interp.h
@@ -3,70 +3,72 @@
 
 #include <stdbool.h>
 
-extern int cFMS_create_xgrid_2dx2d_order1(int *nlon_in, int *nlat_in, int *nlon_out, int *nlat_out,
-                                          double *lon_in, double *lat_in, double *lon_out, double *lat_out,
-                                          double *mask_in, int *maxxgrid, int *i_in, int *j_int, int *i_out,
-                                          int *j_out, double *xgrid_area);
+extern int cFMS_create_xgrid_2dx2d_order1(int* nlon_in, int* nlat_in, int* nlon_out, int* nlat_out,
+    double* lon_in, double* lat_in, double* lon_out, double* lat_out,
+    double* mask_in, int* maxxgrid, int* i_in, int* j_int, int* i_out,
+    int* j_out, double* xgrid_area);
 
 extern int cFMS_get_maxxgrid();
 
-extern void cFMS_horiz_interp_init(int *ninterp);
+extern void cFMS_horiz_interp_init(int* ninterp);
 
 extern void cFMS_horiz_interp_end();
 
-extern int cFMS_horiz_interp_new_2d_cfloat(int *nlon_in, int *nlat_in, int *nlon_out, int *nlat_out,
-                                           float *lon_in_ptr,float *lat_in_ptr, float *lon_out_ptr,
-                                           float *lat_out_ptr, float *mask_in_ptr, float *mask_out_ptr,
-                                           char *interp_method, int *verbose, float *max_dist, bool *src_modulo, 
-                                           bool *is_latlon_in, bool *is_latlon_out, bool *convert_cf_order);
+extern int cFMS_horiz_interp_new_2d_cfloat(int* nlon_in, int* nlat_in, int* nlon_out, int* nlat_out,
+    float* lon_in_ptr, float* lat_in_ptr, float* lon_out_ptr,
+    float* lat_out_ptr, float* mask_in_ptr, float* mask_out_ptr,
+    char* interp_method, int* verbose, float* max_dist, bool* src_modulo,
+    bool* is_latlon_in, bool* is_latlon_out, bool* convert_cf_order);
 
-extern int cFMS_horiz_interp_new_2d_cdouble(int *nlon_in, int *nlat_in, int *nlon_out, int *nlat_out,
-                                            double *lon_in_ptr,double *lat_in_ptr, double *lon_out_ptr,
-                                            double *lat_out_ptr, double *mask_in_ptr, double *mask_out_ptr,
-                                            char *interp_method, int *verbose, double *max_dist, bool *src_modulo, 
-                                            bool *is_latlon_in, bool *is_latlon_out, bool *convert_cf_order);
+extern int cFMS_horiz_interp_new_2d_cdouble(int* nlon_in, int* nlat_in, int* nlon_out, int* nlat_out,
+    double* lon_in_ptr, double* lat_in_ptr, double* lon_out_ptr,
+    double* lat_out_ptr, double* mask_in_ptr, double* mask_out_ptr,
+    char* interp_method, int* verbose, double* max_dist, bool* src_modulo,
+    bool* is_latlon_in, bool* is_latlon_out, bool* convert_cf_order);
 
-extern void cFMS_horiz_interp_base_2d_cfloat(int *interp_id, float *data_in_ptr, float *data_out_ptr, 
-                                             float *mask_in_ptr, float *mask_out_ptr, int *verbose,
-                                             float *missing_value, int *missing_permit, bool *new_missing_handle,
-                                             bool *convert_cf_order);
+extern void cFMS_horiz_interp_base_2d_cfloat(int* interp_id, float* data_in_ptr, float* data_out_ptr,
+    float* mask_in_ptr, float* mask_out_ptr, int* verbose,
+    float* missing_value, int* missing_permit, bool* new_missing_handle,
+    bool* convert_cf_order);
 
-extern void cFMS_horiz_interp_base_2d_cdouble(int *interp_id, double *data_in_ptr, double *data_out_ptr,
-                                              double *mask_in_ptr, double *mask_out_ptr, int *verbose,
-                                              double *missing_value, int *missing_permit, bool *new_missing_handle,
-                                              bool *convert_cf_order);
+extern void cFMS_horiz_interp_base_2d_cdouble(int* interp_id, double* data_in_ptr, double* data_out_ptr,
+    double* mask_in_ptr, double* mask_out_ptr, int* verbose,
+    double* missing_value, int* missing_permit, bool* new_missing_handle,
+    bool* convert_cf_order);
 
-extern int cFMS_horiz_interp_read_weights_conserve(char *weight_filename, char *weight_file_source,
-                                                   int *nlon_src, int *nlat_src, int *nlon_dst, int *nlat_dst,
-                                                   int *isw, int *iew, int *jsw, int *jew, int *src_tile);
+extern int cFMS_horiz_interp_read_weights_conserve(char* weight_filename, char* weight_file_source,
+    int* nlon_src, int* nlat_src, int* nlon_dst, int* nlat_dst,
+    int* isw, int* iew, int* jsw, int* jew, int* src_tile);
 
 // Integer pointer getters
-extern void cFMS_get_i_src(int *interp_id, int *i_src);
-extern void cFMS_get_j_src(int *interp_id, int *j_src);
-extern void cFMS_get_i_dst(int *interp_id, int *i_dst);
-extern void cFMS_get_j_dst(int *interp_id, int *j_dst);
-extern void cFMS_get_i_lon(int *interp_id, int *i_lon);
-extern void cFMS_get_j_lat(int *interp_id, int *j_lat);
+extern void cFMS_get_i_src(int* interp_id, int* i_src);
+extern void cFMS_get_j_src(int* interp_id, int* j_src);
+extern void cFMS_get_i_dst(int* interp_id, int* i_dst);
+extern void cFMS_get_j_dst(int* interp_id, int* j_dst);
+extern void cFMS_get_i_lon(int* interp_id, int* i_lon);
+extern void cFMS_get_j_lat(int* interp_id, int* j_lat);
 
 // float/double pointer getters
-extern void cFMS_get_area_frac_dst_cfloat(int *interp_id, float *area_frac_dst);
-extern void cFMS_get_area_frac_dst_cdouble(int *interp_id, double *area_frac_dst);
-extern void cFMS_get_wti_cdouble(int *interp_id, double *wti);
-extern void cFMS_get_wti_cfloat(int *interp_id, float *wti);
-extern void cFMS_get_wtj_cdouble(int *interp_id, double *wtj);
-extern void cFMS_get_wtj_cfloat(int *interp_id, float *wtj);
-extern void cFMS_get_is_allocated_cdouble(int *interp_id, bool *is_allocated);
-extern void cFMS_get_is_allocated_cfloat(int *interp_id, bool *is_allocated);
+extern void cFMS_get_area_frac_dst_cfloat(int* interp_id, float* area_frac_dst);
+extern void cFMS_get_area_frac_dst_cdouble(int* interp_id, double* area_frac_dst);
+extern void cFMS_get_xgrid_area_cfloat(int* interp_id, float* xgrid_area);
+extern void cFMS_get_xgrid_area_cdouble(int* interp_id, double* xgrid_area);
+extern void cFMS_get_wti_cdouble(int* interp_id, double* wti);
+extern void cFMS_get_wti_cfloat(int* interp_id, float* wti);
+extern void cFMS_get_wtj_cdouble(int* interp_id, double* wtj);
+extern void cFMS_get_wtj_cfloat(int* interp_id, float* wtj);
+extern void cFMS_get_is_allocated_cdouble(int* interp_id, bool* is_allocated);
+extern void cFMS_get_is_allocated_cfloat(int* interp_id, bool* is_allocated);
 
 // Scalar getters
-extern void cFMS_get_version(int *interp_id, int *version);
-extern void cFMS_get_nxgrid(int *interp_id, int *nxgrid);
-extern void cFMS_get_nlon_src(int *interp_id, int *nlon_src);
-extern void cFMS_get_nlat_src(int *interp_id, int *nlat_src);
-extern void cFMS_get_nlon_dst(int *interp_id, int *nlon_dst);
-extern void cFMS_get_nlat_dst(int *interp_id, int *nlat_dst);
-extern void cFMS_get_is_allocated(int *interp_id, bool *is_allocated);
-extern void cFMS_get_interp_method(int *interp_id, int *interp_method);
+extern void cFMS_get_version(int* interp_id, int* version);
+extern void cFMS_get_nxgrid(int* interp_id, int* nxgrid);
+extern void cFMS_get_nlon_src(int* interp_id, int* nlon_src);
+extern void cFMS_get_nlat_src(int* interp_id, int* nlat_src);
+extern void cFMS_get_nlon_dst(int* interp_id, int* nlon_dst);
+extern void cFMS_get_nlat_dst(int* interp_id, int* nlat_dst);
+extern void cFMS_get_is_allocated(int* interp_id, bool* is_allocated);
+extern void cFMS_get_interp_method(int* interp_id, int* interp_method);
 
 
 #endif
